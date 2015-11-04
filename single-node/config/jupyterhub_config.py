584834ace0977b201499e33f3e5517b360f74698
Enric Tejedor Saavedra <lenric@gmail.com>
2015-11-04 16:58:54 +0100

    Simplify JupyterHub configuration to the minimum

--- single-node/config/jupyterhub_config.py
+++ single-node/config/jupyterhub_config.py
@@ -6,17 +6,6 @@ from IPython.utils.localinterfaces import public_ips
 c.JupyterHub.hub_ip = public_ips()[0]
 c.JupyterHub.port = 8000 
 
-# SSL
-#c.JupyterHub.port = 443
-#pem_dir   = '/srv/jupyterhub/certs/'
-#c.JupyterHub.ssl_cert = pem_dir + 'jh.crt'
-#c.JupyterHub.ssl_key  = pem_dir + 'jh.pem' 
-
-# Authenticator
-c.Authenticator.admin_users = {'etejedor', 'dpiparo', 'moscicki', 'mascetti'}
-
 # Spawner
 c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
 c.SystemUserSpawner.container_image = 'cernphsft/systemuser'
-#c.SystemUserSpawner.read_only_volumes = { '/opt/ROOT/':'/opt/ROOT/',
-#                                          '/opt/x86_64-slc6-gcc48-opt/' : '/opt/x86_64-slc6-gcc48-opt/' }
